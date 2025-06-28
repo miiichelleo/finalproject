a684ab0def9194544402c1e0314a487c5ea43a81
miiichelleo <michelle.otiji@fh-potsdam.de>
2025-06-28 16:55:36 +0200

    28.06

--- Alarmsysteme/sketch.js
+++ Alarmsysteme/sketch.js
@@ -4,9 +4,9 @@ let fft;
 
 let capturer;
 let recording = false;
-let maxFrames = 800; // 10 seconds at 60 FPS
+let maxFrames = 100; // 10 seconds at 60 FPS
 
-let resolution =60; // Sphere detail (lat/lon divisions)
+let resolution =80; // Sphere detail (lat/lon divisions)
 
 function setup() {
   createCanvas(600, 600, WEBGL);
@@ -29,7 +29,7 @@ function setup() {
   // Setup CCapture for recording
   capturer = new CCapture({
     format: 'webm',
-    framerate: 60,
+    framerate: 50,
     verbose: true,
   });
 
@@ -46,7 +46,7 @@ function keyPressed() {
 }
 
 function draw() {
-  background(255, 18.0);
+  background(255, 10);
 
   rotateY(frameCount * 0.01); // Spin the globe
   rotateX(PI / 10); // Tilt for better view
@@ -58,27 +58,32 @@ function draw() {
 
   // Draw frequency-reactive particles on globe
   fill(0);
-  for (let lat = 0; lat < resolution; lat++) {
-    let theta = map(lat, 100, resolution, -HALF_PI, HALF_PI);
-    for (let lon = -1; lon < resolution * 2; lon++) {
+  for (let lat = 1; lat < resolution; lat++) {
+    let theta = map(lat, 0, resolution, -HALF_PI, HALF_PI);
+    for (let lon = 1; lon < resolution * 2; lon++) {
       let phi = map(lon, 0, resolution * 2, 0, TWO_PI);
       let idx = (lat * resolution + lon) % spectrum.length;
       let ampVal = spectrum[idx];
-      let r = baseRadius + map(ampVal, 0, 300, 0, 100);
 
+      let droop = map(ampVal, 0, 1000, 10, 80);
+
+      let r = baseRadius + map(ampVal, 10, 340, 10, 200);
       let x = r * cos(theta) * cos(phi);
-      let y = r * sin(theta);
+      let y = r * sin(theta) + droop;
       let z = r * cos(theta) * sin(phi);
+
+      let alpha = map(droop, 0, 80, 255, 100);
+      fill(0, alpha);
 
       push();
       translate(x, y, z);
-      sphere(1); // small reactive dot
+      sphere(0.8); // small reactive dot
       pop();
     }
   }
 
   // Draw waveform-reactive ring around globe's equator
-  fill(0, 5); // faint black
+  fill(0, 20); // faint black
   let ringRadius = baseRadius + -10;
   for (let i = 0; i < waveform.length; i++) {
     let angle = map(i, 0, waveform.length, 0, TWO_PI);
@@ -90,7 +95,7 @@ function draw() {
 
     push();
     translate(x, y, z);
-    sphere(2); // waveform ring dot
+    sphere(10); // waveform ring dot
     pop();
   }
 
